1769bcc504e04a02a4765aba0bd51f7ede01250a
linxiaomo <sunnymo56@gmail.com>
2015-07-13 18:53:04 +0800

    first commit

---
+++ js/online-chat.js
@@ -0,0 +1,8 @@
+//#@+livechat-zenith-content
+document.write('<script src="http://pkt.zoosnet.net/JS/LsJS.aspx?siteid=PKT56764896&float=1&lng=en"><\/script>');   
+//#@-livechat-zenith-content
+
+
+document.oncontextmenu=new Function("event.returnValue=false");
+
+document.onselectstart=new Function("event.returnValue=false");
